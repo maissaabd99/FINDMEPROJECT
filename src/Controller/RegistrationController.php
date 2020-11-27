d3a6dccfe2bf5997bdfc7a36d885c94a0efd162e
YOUR NAME <YOUR EMAIL ADDRESS>
2020-11-27 14:18:49 +0100

    sprint1

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use App\Form\RegistrationFormType;
+use App\Repository\UtilisateurRepository;
+use App\Service\Mailer;
+use Exception;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+
+    /**
+     * @var Mailer
+     */
+    private $mailer;
+    public function __construct(Mailer $mailer){
+        $this->mailer=$mailer;
+    }
+
+
+    /**
+     * @Route("/register", name="app_register")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return Response
+     * @throws Exception
+     */
+
+
+
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = new Utilisateur();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+//            $user->setToken($this->generateToken());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->mailer->senEmail($user->getEmail(),$form->get('nom')->getData());
+            return $this->redirectToRoute('verification');
+        }
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/mon-compte/modifier", name="modifiercompte")
+     * @param Request $request
+     * @param UtilisateurRepository $repository
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return Response
+     */
+
+
+
+    public function modifiercompte(Request $request,UtilisateurRepository $repository, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = $repository->find($this->getUser()->getId());
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->remove('agreeTerms');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+//            $user->setToken($this->generateToken());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+//            $this->mailer->senEmail($user->getEmail(),$form->get('nom')->getData());
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('compte.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
